687c4215055e8c0013f0c286e8d81ed66c3edfa4
Maxim Tebenev <mtebenev@gmail.com>
2017-11-09 06:30:11 +0300

    Added company list razor page

---
+++ ServerAppMvc.Web/Pages/Companies/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mt.WebVNext.ServerAppMvc.Web.Companies
+{
+  public class IndexModel : PageModel
+  {
+    public void OnGet()
+    {
+    }
+  }
+}
