d6a556e4d446337f77814b3072487e81f85ec174
Maxim Tebenev <mtebenev@gmail.com>
2018-01-03 11:48:56 +0300

    Angular: layout fixes

--- ClientApp.Angular/src/app/app.component.ts
+++ ClientApp.Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, ViewChild} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {MediaChange, ObservableMedia} from '@angular/flex-layout';
 
@@ -6,6 +7,7 @@ import {MatSidenav} from '@angular/material';
 import {TranslateService} from '@ngx-translate/core';
 
 import {ISubscription} from 'rxjs/Subscription';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-root',
@@ -14,19 +16,27 @@ import {ISubscription} from 'rxjs/Subscription';
 })
 export class AppComponent {
 
-  private _mediaSubscription: ISubscription;
+  private _subscriptionRouter: ISubscription;
+  private _subscriptionMedia: ISubscription;
   private _isMobileMode: boolean;
 
-  constructor(private httpClient: HttpClient, private translateService: TranslateService, private media: ObservableMedia) {
+  constructor(private httpClient: HttpClient, private translateService: TranslateService, private media: ObservableMedia, router: Router) {
 
     translateService.setDefaultLang('en');
     translateService.use('en');
 
     // Listen to media change to redraw sidebar
     // Note MTE: in original Material sample there's ChangeDetectorRef used. Really need that?
-    this._mediaSubscription = media.subscribe((mediaChange: MediaChange) => {
+    this._subscriptionMedia = media.subscribe((mediaChange: MediaChange) => {
       this._isMobileMode = this.media.isActive('lt-sm');
     });
+
+    // Listen router and hide nav sidebar when navigation occurres
+    this._subscriptionRouter = router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .subscribe((event) => {
+        this.onNavigationEnd();
+      });
 
     this._isMobileMode = this.media.isActive('lt-sm'); // Initial layout
   }
@@ -39,8 +49,12 @@ export class AppComponent {
    */
   public ngOnDestroy(): void {
 
-    if (this._mediaSubscription) {
-      this._mediaSubscription.unsubscribe();
+    if(this._subscriptionMedia) {
+      this._subscriptionMedia.unsubscribe();
+    }
+
+    if(this._subscriptionRouter) {
+      this._subscriptionRouter.unsubscribe();
     }
   }
 
@@ -56,5 +70,13 @@ export class AppComponent {
    */
   public handleMenuButtonClick(): void {
     this.sideNav.toggle();
+  }
+
+  private onNavigationEnd(): void {
+
+    // For mobiles close sidenav after navigation
+    if(this._isMobileMode) {
+      this.sideNav.close();
+    }
   }
 }
