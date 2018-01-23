44403971f7181b7636bc6461e9c637b24d3e6811
Maxim Tebenev <mtebenev@gmail.com>
2018-01-23 14:23:48 +0300

    Angular: test for paged request in company list

--- ClientApp.Angular/src/app/contact-manager/company-list.component.spec.ts
+++ ClientApp.Angular/src/app/contact-manager/company-list.component.spec.ts
@@ -1,7 +1,43 @@
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+
+import {SharedModule} from '@shared/shared.module';
+import {CompanyListComponent} from './company-list.component';
+import {CompanyHttpService} from '@http-services/contact-manager/company-http.service';
+
+let fixture: ComponentFixture<CompanyListComponent>;
+
 describe('CompanyListComponent', () => {
 
-  it('Performs a simple test', () => {
-    expect(true).toBeTruthy();
+  beforeEach(async(() => {
+
+    let mockCompanyHttp = jasmine.createSpyObj<CompanyHttpService>('CompanyHttpService', ['getCompanies']);
+
+    TestBed.configureTestingModule({
+      declarations: [
+        CompanyListComponent,
+      ],
+      imports: [
+        SharedModule,
+        RouterTestingModule
+      ],
+      providers: [
+        {
+          provide: CompanyHttpService, useValue: mockCompanyHttp
+        },
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CompanyListComponent);
+  }));
+
+  it('Should perform paged request', () => {
+
+    let componentInstance = fixture.componentInstance;
+    componentInstance.handlePageChange({pageIndex: 2, pageSize: 20, length: 100});
+
+    let mockCompanyHttp: jasmine.SpyObj<CompanyHttpService> = TestBed.get(CompanyHttpService);
+    expect(mockCompanyHttp.getCompanies).toHaveBeenCalledWith(jasmine.objectContaining({pageNumber: 2, pageSize: 20}));
   });
 
 });
