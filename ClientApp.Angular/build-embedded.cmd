1c53912baa02c403f467545cab1e2334032ee784
Maxim Tebenev <mtebenev@gmail.com>
2018-04-21 22:52:03 +0300

    Updated environment settings

---
+++ ClientApp.Angular/build-embedded.cmd
@@ -0,0 +1,1 @@
+npx ng build --base-href "/angular/" --deploy-url "/.Modules/ClientApp.Angular/dist/" --environment dev-embedded
---
+++ ClientApp.Angular/src/environments/environment.dev-embedded.ts
@@ -0,0 +1,12 @@
+import {IEnvironment} from './ienvironment';
+
+export const environment: IEnvironment = {
+  production: false,
+  identityServerConfig: {
+    clientId: 'angularclient_hosted',
+    clientBaseUrl: 'http://localhost:5200/angular',
+    serverUrl: 'http://localhost:3200'
+  },
+  apiBaseUrl: 'http://localhost:5200/api',
+  translationsPathPrefix: '/.Modules/ClientApp.Angular/dist/assets/i18n/'
+};
