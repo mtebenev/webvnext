a94d20dcfde33d9a60621253445809ead471e389
Maxim Tebenev <mtebenev@gmail.com>
2018-06-02 06:18:59 +0300

    React: merge conflict fix

--- ClientApp.React/src/app/core/mui-exports.ts
+++ ClientApp.React/src/app/core/mui-exports.ts
@@ -17,6 +17,12 @@ import ListItemText from '@material-ui/core/ListItemText';
 
 import TextField from '@material-ui/core/TextField';
 import Toolbar from '@material-ui/core/Toolbar';
+
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+
 import Typography from '@material-ui/core/Typography';
 import {withStyles, WithStyles, Theme} from '@material-ui/core/styles';
 import {Hidden} from '@material-ui/core';
