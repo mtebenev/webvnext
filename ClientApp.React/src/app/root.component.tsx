31e607a58a9d0ac45cd8938b3318bdf1b883c05f
Maxim Tebenev <mtebenev@gmail.com>
2018-09-14 13:31:22 +0300

    React: fixing migration issues

--- ClientApp.React/src/app/root.component.tsx
+++ ClientApp.React/src/app/root.component.tsx
@@ -1,29 +1,32 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import * as i18n from 'i18next'
-import withStyles, {WithStyles, StyleRulesCallback} from '@material-ui/core/styles/withStyles';
+import withStyles, {WithStyles} from '@material-ui/core/styles/withStyles';
 import withRoot from './withRoot';
+import createStyles from '@material-ui/core/styles/createStyles';
+import {Theme} from '@material-ui/core/styles/createMuiTheme';
 import {AppComponent} from './app.component';
 import {I18nextProvider} from 'react-i18next';
 
-const styles: StyleRulesCallback<'root'> = theme => ({
-  root: {
-  },
-  '@global': {
-    html: {
-      width: '100%',
-      height: '100%'
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
     },
-    body: {
-      width: '100%',
-      height: '100%'
-    },
-    '#root': {
-      width: '100%',
-      height: '100%'
+    '@global': {
+      html: {
+        width: '100%',
+        height: '100%'
+      },
+      body: {
+        width: '100%',
+        height: '100%'
+      },
+      '#root': {
+        width: '100%',
+        height: '100%'
+      }
     }
-  }
-});
+  });
 
 interface IIntlContext {
   i18n: i18n.i18n;
@@ -36,16 +39,16 @@ interface IState {
 /**
  * Encapsulates app component initialization
  */
-class RootComponent extends React.Component<WithStyles<'root'>, IState> {
+class RootComponent extends React.Component<WithStyles<typeof styles>, IState> {
 
   public static contextTypes: PropTypes.ValidationMap<IIntlContext> = {
     i18n: PropTypes.object
   };
 
-  constructor(props: any, context: IIntlContext) {
+  constructor(props: any, context?: IIntlContext) {
     super(props);
 
-    context.i18n.on('initialized', (options: i18n.InitOptions) => {
+    context!.i18n.on('initialized', (options: i18n.InitOptions) => {
       this.setState({isTranslationLoaded: true});
     });
 
@@ -64,4 +67,4 @@ class RootComponent extends React.Component<WithStyles<'root'>, IState> {
   }
 }
 
-export default withRoot(withStyles(styles)<{}>(RootComponent));
+export default withRoot(withStyles(styles)(RootComponent));
--- ClientApp.React/src/app/withRoot.tsx
+++ ClientApp.React/src/app/withRoot.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
+import indigo from '@material-ui/core/colors/indigo';
+import pink from '@material-ui/core/colors/pink';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 // c/p from https://github.com/mui-org/material-ui/blob/master/examples/create-react-app-with-typescript/src/withRoot.tsx
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: pink,
+  },
+});
 
-function withRoot(Component: React.ComponentType): any {
+function withRoot<P>(Component: React.ComponentType): any {
 
-  function WithRoot(props: object): React.ReactNode {
+  function WithRoot(props: P): React.ReactNode {
     // MuiThemeProvider makes the theme available down the React tree
     // thanks to React context.
     return (
